122f12e99431b51d7356111cb8cd110d435fc968
gatu <65786746+atcnaked@users.noreply.github.com>
2026-06-16 07:06:53 +0200

    Create result_pattern_with_factory.dart

---
+++ result_pattern_with_factory.dart
@@ -0,0 +1,19 @@
+sealed class Result<T> {
+  const Result();
+
+  factory Result.ok(T value) => Ok._(value);
+
+  factory Result.error(Exception error) => Error._(error);
+}
+
+final class Ok<T> extends Result<T> {
+ const Ok._(this.value);
+
+  final T value;
+}
+
+final class Error<T> extends Result<T> {
+ const Error._(this.error);
+
+  final Exception error;
+}
